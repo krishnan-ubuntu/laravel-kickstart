8cf791d3147eed91315c40aefd6bb755057cffef
Krishnan <krishnan.ubuntu@gmail.com>
2023-10-01 13:15:11 +0530

    Basic kickstart scaffolding

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -8,4 +8,23 @@
     <li>Logout</li>
     <li>Logout API</li>
     <li>Free Bootstrap theme for web app</li>
+    <li>Logged user profile</li>
+    <li>Change password</li>
+    <li>Create user</li>
+    <li>Manage users</li>
+    <li>Create user API</li>
+    <li>Manage users API</li>
+    <li>Edit user</li>
+    <li>Delete user</li>
+    <li>Edit user API</li>
+    <li>Delete user API</li>
+    <li>Admin console
+        <ol>
+            <li>Change timezone</li>
+            <li>Change date format</li>
+        </ol>
+    </li>
+    <li>API auth with JWT</li>
+    <li>Session check middleware</li>
+    <li>Admin check middleware (session and api)</li>
 </ol>
